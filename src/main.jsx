8b164dd8ff7ba147e84938800e0a937bcb23163f
umerbaig9922 <muhammadumerbaig9922@gmail.com>
2025-04-27 23:21:15 +0500

    Adds routes to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import AboutPage from "./AboutPage.jsx";
+import PublicationsPage from "./PublicationsPage.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<AboutPage />} />
+      <Route path="/publications" element={<PublicationsPage />} />
+    </Routes>
+  </BrowserRouter>
+);
