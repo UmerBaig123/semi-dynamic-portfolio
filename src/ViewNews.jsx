d1527c5b926b92ac765dc3d59b7008d300535612
umerbaig9922 <muhammadumerbaig9922@gmail.com>
2025-05-01 22:31:18 +0500

    Adds PDF file and updates news data to include descriptions and links; enhances ViewNews component with improved layout and error handling.

--- src/ViewNews.jsx
+++ src/ViewNews.jsx
@@ -1,17 +1,64 @@
 import { useParams } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
-import { useEffect } from "react";
-
+import { useEffect, useState } from "react";
+import "./ViewNews.css";
 const ViewNews = () => {
   const { news_id } = useParams();
+  const [news, setNews] = useState(null);
   useEffect(() => {
-    console.log("news_id", news_id);
+    fetch("/data/news/data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        const news = data.find((item) => item.id === parseInt(news_id));
+        setNews(news);
+        if (news) {
+          console.log(news);
+        } else {
+          console.error("News not found");
+        }
+      })
+      .catch((error) => console.error("Error fetching news:", error));
   }, [news_id]);
-
+  if (!news) {
+    return <div>News not found</div>;
+  }
   return (
     <>
-      <Navbar />
-      <div className="container">News id {news_id}</div>
+      <div style={{ maxWidth: "90vw" }}>
+        <Navbar />
+        <div className="container">
+          <div className="title">{news.title}</div>
+        </div>
+        <div className="date">{news.date}</div>
+        <div className="header" style={{ marginTop: "20px" }}>
+          <img
+            style={{
+              maxWidth: "90%",
+              boxShadow: "0 0 10px 0 rgba(0, 0, 0, 0.507)",
+            }}
+            src={news.image}
+            alt="news image"
+          />
+        </div>
+        <div className="desc" id="description">
+          {news.description}
+        </div>
+
+        <div
+          className="title"
+          style={{ margin: "20px 0", fontSize: "30px", paddingTop: "20px" }}
+        >
+          File
+        </div>
+        <div className="header">
+          <iframe
+            src={news.pdf}
+            width="100%"
+            height="565px"
+            frameBorder="0"
+          ></iframe>
+        </div>
+      </div>
     </>
   );
 };
