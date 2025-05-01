1eed789d03fee8817c34f4890246fff753a9e631
umerbaig9922 <muhammadumerbaig9922@gmail.com>
2025-05-01 20:54:14 +0500

    Adds theme (light mode / dark mode) to context over all components

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AboutPage from "./AboutPage.jsx";
 import PublicationsPage from "./PublicationsPage.jsx";
 import "./index.css";
-
+import { ThemeProvider } from "./ThemeContextProvider.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
+  <ThemeProvider>
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<AboutPage />} />
       <Route path="/publications" element={<PublicationsPage />} />
     </Routes>
   </BrowserRouter>
+  </ThemeProvider>
 );
