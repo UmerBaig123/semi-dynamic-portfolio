cc6ac3afac46d6478ad7f0c5b85aca253488233e
umerbaig9922 <muhammadumerbaig9922@gmail.com>
2025-05-01 20:54:46 +0500

    Adds functionality in NavBar to switch themes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,31 @@
+import { useEffect, useState, useContext } from "react";
 import "./Navbar.css";
+import { ThemeContext } from "../../ThemeContextProvider";
 const Navbar = () => {
+  const { isDarkMode, setIsDarkMode } = useContext(ThemeContext);
+  function updateCSSVariables(variables) {
+    const root = document.documentElement;
+
+    for (const [key, value] of Object.entries(variables)) {
+      // Convert snake_case to CSS variable format (e.g., navbar_bg -> --navbar-bg)
+      const cssVarName = "--" + key.replace(/_/g, "-");
+      root.style.setProperty(cssVarName, value);
+    }
+  }
+
+  useEffect(() => {
+    let key = isDarkMode ? "dark" : "light";
+    fetch("/data/general.json")
+      .then((response) => response.json())
+      .then((data) => {
+        const theme = data.theme[key];
+        updateCSSVariables(theme);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+    const checkbox = document.getElementById("checkbox");
+  }, [isDarkMode]);
   return (
     <div className="topbar">
       <div className="options" id="navbarList">
@@ -20,7 +46,11 @@ const Navbar = () => {
         </a>
       </div>
       <label className="switch">
-        <input type="checkbox" id="checkbox" />
+        <input
+          type="checkbox"
+          id="checkbox"
+          onChange={() => setIsDarkMode((mode) => !mode)}
+        />
         <span className="slider"></span>
       </label>
     </div>
