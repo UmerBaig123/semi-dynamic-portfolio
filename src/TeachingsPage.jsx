46254b50a6d8d9e49bff4add2cb88b099876e2e7
umerbaig9922 <muhammadumerbaig9922@gmail.com>
2025-05-01 21:45:21 +0500

    Adds teachings page using aggrid to display the teachings as table

---
+++ src/TeachingsPage.jsx
@@ -0,0 +1,67 @@
+import Navbar from "./components/navbar/Navbar";
+import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
+import { AgGridReact } from "ag-grid-react";
+import { useContext, useEffect, useState } from "react";
+import { themeBalham } from "ag-grid-community";
+import "./TeachingsPage.css";
+import { ThemeContext } from "./ThemeContextProvider";
+ModuleRegistry.registerModules([AllCommunityModule]);
+const TeachingsPage = () => {
+  const { isDarkMode, setIsDarkMode } = useContext(ThemeContext);
+  const [rowData, setRowData] = useState([]);
+  const myTheme = themeBalham.withParams({
+    backgroundColor: isDarkMode ? "rgb(21, 21, 21)" : "rgb(255, 255, 255)",
+    foregroundColor: isDarkMode ? "rgb(20, 20, 20)" : "rgb(255, 255, 255)",
+    headerTextColor: isDarkMode ? "rgb(255, 255, 255)" : "rgb(0, 0, 0)",
+    headerBackgroundColor: isDarkMode
+      ? "rgb(62, 77, 140)"
+      : "rgb(143, 171, 255)",
+    textColor: isDarkMode ? "rgb(255, 255, 255)" : "rgb(0, 0, 0)",
+    oddRowBackgroundColor: isDarkMode
+      ? "rgb(255, 255, 255, 0.10)"
+      : "rgb(0, 0, 0, 0.10)",
+    headerColumnResizeHandleColor: isDarkMode
+      ? "rgb(154, 154, 154)"
+      : "rgb(29, 29, 29)",
+  });
+  const [colDefs, setColDefs] = useState([]);
+  useEffect(() => {
+    fetch("/data/teachings/data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setRowData(data.rows);
+        const colDefs = data.columns.map((col) => {
+          return { field: col, flex: 1, filter: true };
+        });
+        setColDefs(colDefs);
+      });
+  }, []);
+  return (
+    <div className="container">
+      <Navbar />
+      <div className="item-box" style={{ borderTop: "none" }}>
+        <div
+          className="heading"
+          style={{
+            paddingBottom: "50px",
+            width: "100%",
+            borderBottom: "1px solid rgba(255, 255, 255, 0.26)",
+            marginBottom: "10px",
+          }}
+        >
+          Teachings
+        </div>
+
+        <div className="ag-theme-alpine-dark ag-grid-container">
+          <AgGridReact
+            rowData={rowData}
+            columnDefs={colDefs}
+            theme={myTheme}
+            gridOptions={{ domLayout: "autoHeight" }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default TeachingsPage;
